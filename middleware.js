527944923d70e5f90f02bc565ae229e84a38fe84
Md Ratul Hasan <82720031+ratulxhasan@users.noreply.github.com>
2025-09-12 19:23:14 +0600

    Delete middleware.js

--- middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(req) {
-  const host = req.headers.get('host');
-  if (host.endsWith('.vercel.app')) {
-    return new Response('এই ঠিকানায় প্রবেশ নিষিদ্ধ ❌', { status: 403 });
-  }
-  return NextResponse.next();
-}
